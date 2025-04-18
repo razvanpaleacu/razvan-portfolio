3d41309dba4ff056f5761b19b10d503f372799b6
razvanpaleacu <39214150+razvanpaleacu@users.noreply.github.com>
2025-04-18 23:02:25 +0300

    Initial commit for Razvan portfolio 🔥

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['"IBM Plex Mono"', 'monospace'],
+      },
+      colors: {
+        dark: '#0e0e10',
+        magenta: '#ff00cc',
+      },
+    },
+  },
+  plugins: [],
+}
